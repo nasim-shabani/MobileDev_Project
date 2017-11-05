ebbfe4a69ee503092b85784c0292e8f5b279abb0
Daniela Lupo <daniela.lupo@student.pxl.be>
2017-11-05 02:29:08 +0100

    string between fragment

--- Hasseling/app/src/main/java/be/pxl/hasseling/activity/SupermarketDetailsActivity.java
+++ Hasseling/app/src/main/java/be/pxl/hasseling/activity/SupermarketDetailsActivity.java
@@ -1,18 +1,7 @@
 package be.pxl.hasseling.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import be.pxl.hasseling.R;
 
@@ -25,29 +14,4 @@ public class SupermarketDetailsActivity extends AppCompatActivity {
 
     }
 
-    public static class SupermarketDetailsActivityFragment extends Fragment {
-
-        private String supermarketStr;
-
-        public SupermarketDetailsActivityFragment() {
-
-
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_supermarket_details, container, false);
-            // The detail Activity called via intent.  Inspect the intent for forecast data.
-            Intent intent = getActivity().getIntent();
-            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                supermarketStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((TextView) rootView.findViewById(R.id.supermarketdetail_text))
-                        .setText(supermarketStr);
-            }
-
-            return rootView;
-        }
-
-    }
 }
